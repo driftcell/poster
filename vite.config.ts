154e34f505be7520160dd2d2cf7faf8cb7e68521
driftcell <driftcell@icloud.com>
2026-08-19 11:25:07 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+
+			// Build for Cloudflare Workers with Static Assets.
+			// See https://svelte.dev/docs/kit/adapter-cloudflare for more information.
+			adapter: adapter()
+		})
+	]
+});
